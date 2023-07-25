8a5b7c4e3d71cf934808d4647c04ff52037f35af
ptnoire <ptnoire@gmail.com>
2023-07-25 07:07:04 -0400

    Begin the good work

---
+++ components/loading.tsx
@@ -0,0 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+export const LoadingSpinner = () => {
+  return <FontAwesomeIcon icon={faSpinner} className="fa-icon spinner" />;
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,47 @@
 import { type AppType } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import Head from "next/head";
+import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Listwiser</title>
+        <meta name="description" content="Bill Tracking" />
+        <meta
+          name="keywords"
+          content="Payd-2, Payd, Bill Tracking, Bill, Tracker, Simple, Simple Bill Tracking, Bill Tracker, Bills, Finance"
+        ></meta>
+        <link
+          rel="icon"
+          media="(prefers-color-scheme: light)"
+          href="/ptblack.ico"
+        />
+        <link
+          rel="icon"
+          media="(prefers-color-scheme: dark)"
+          href="/ptwhite.ico"
+        />
+      </Head>
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          style: {
+            border: "1px solid #00C853",
+            background: "#14150b",
+            color: "#f5f5dc",
+          },
+          error: {
+            style: { border: "1px solid #e91313" },
+          },
+        }}
+      />
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
